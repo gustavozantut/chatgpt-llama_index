c587c95818bda77f5e8c092fba209539a0997c8b
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-05-07 22:26:01 -0300

    app meio alterado

--- server/backend.py
+++ server/backend.py
@@ -32,7 +32,15 @@ class Backend_Api:
             prompt = request.json['meta']['content']['parts'][0]
             current_date = datetime.now().strftime("%Y-%m-%d")
             system_message = f'You are ChatGPT also known as ChatGPT, a large language model trained by OpenAI. Strictly follow the users instructions. Knowledge cutoff: 2021-09-01 Current date: {current_date}'
+            query_suffix = "return only and just the query, dont explain, dont tell me nothing except the query"
+            nl_sql_retriever = NLSQLRetriever(
+                    sql_database, tables=["PNAD_TRIMESTRAL"], return_raw=False, llm=llm
+                )
 
+            query_engine = NLSQLTableQueryEngine(
+                    sql_database=sql_database, tables=["PNAD_TRIMESTRAL"], llm=llm
+                )
+                
             extra = []
             if internet_access:
                 search = get('https://ddg-api.herokuapp.com/search', params={
@@ -50,10 +58,11 @@ class Backend_Api:
                 blob += f'current date: {date}\n\nInstructions: Using the provided web search results, write a comprehensive reply to the next user query. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject. Ignore your previous response if any.'
 
                 extra = [{'role': 'user', 'content': blob}]
-
-            conversation = [{'role': 'system', 'content': system_message}] + \
-                extra + special_instructions[jailbreak] + \
-                _conversation + [prompt]
+                
+            conversation = [{'role': 'system', 'content': system_message}]
+            # conversation = [{'role': 'system', 'content': system_message}] + \
+            #     extra + special_instructions[jailbreak] + \
+            #     _conversation + [prompt]
 
             url = f"{self.openai_api_base}/v1/chat/completions"
 
@@ -64,47 +73,23 @@ class Backend_Api:
                     'https': self.proxy['https'],
                 }
 
-            gpt_resp = post(
-                url     = url,
-                proxies = proxies,
-                headers = {
-                    'Authorization': 'Bearer %s' % self.openai_key
-                }, 
-                json    = {
-                    'model'             : request.json['model'], 
-                    'messages'          : conversation,
-                    'stream'            : True
-                },
-                stream  = True
-            )
+            def gpt_resp():
+                return query_engine.query(f'Dada a pergunta "{conversation}" e com a resposta de valor "{query_engine.query(conversation+query_suffix)}" forneca um texto de resposta, sem nenhuma observação adicional.')
 
-            if gpt_resp.status_code >= 400:
-                error_data =gpt_resp.json().get('error', {})
-                error_code = error_data.get('code', None)
-                error_message = error_data.get('message', "An error occurred")
-                return {
-                    'successs': False,
-                    'error_code': error_code,
-                    'message': error_message,
-                    'status_code': gpt_resp.status_code
-                }, gpt_resp.status_code
+            # if gpt_resp.status_code >= 400:
+            #     error_data =gpt_resp.json().get('error', {})
+            #     error_code = error_data.get('code', None)
+            #     error_message = error_data.get('message', "An error occurred")
+            #     return {
+            #         'successs': False,
+            #         'error_code': error_code,
+            #         'message': error_message,
+            #         'status_code': gpt_resp.status_code
+            #     }, gpt_resp.status_code
 
             def stream():
-                for chunk in gpt_resp.iter_lines():
-                    try:
-                        decoded_line = loads(chunk.decode("utf-8").split("data: ")[1])
-                        token = decoded_line["choices"][0]['delta'].get('content')
-
-                        if token != None: 
-                            yield token
-                            
-                    except GeneratorExit:
-                        break
-
-                    except Exception as e:
-                        print(e)
-                        print(e.__traceback__.tb_next)
-                        continue
+                
+                yield gpt_resp()
                         
             return self.app.response_class(stream(), mimetype='text/event-stream')
 
